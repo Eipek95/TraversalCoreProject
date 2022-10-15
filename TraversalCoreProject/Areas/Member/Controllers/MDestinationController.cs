b6792dd2debf91bf9a63df9291eb43efc841fb96
Eipek95 <eipek1995@gmail.com>
2022-10-15 21:42:00 +0300

    Add Member Page

---
+++ TraversalCoreProject/Areas/Member/Controllers/MDestinationController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class MDestinationController : Controller
+    {
+        DestinationManager destinationManage = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var result = destinationManage.GetAll();
+            return View(result.Data);
+        }
+    }
+}
