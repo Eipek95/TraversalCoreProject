1b036ef1001fddae2a75cee568d7386404f872c4
Eipek95 <eipek1995@gmail.com>
2022-10-18 16:17:27 +0300

    Admin Dashboard

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_AdminDashboardHeaderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _AdminDashboardHeaderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
